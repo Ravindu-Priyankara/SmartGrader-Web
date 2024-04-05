533ab58d99820d776f185a90ed648a1d2797b3a5
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2024-04-05 10:50:02 +0530

    add login route to landing page

--- smartGrader/webApp/urls.py
+++ smartGrader/webApp/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 #url patterns
 urlpatterns = [
-    path('',views.index,name='index'), #index page path(landing page)
+    path('',views.index, name='index'), #index page path(landing page)
+    path('login/',views.login, name = 'login'), #login page path(landing page)
 ]
