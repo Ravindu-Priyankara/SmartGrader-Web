62602ed266a7faf01b57cf7b53031da7ad802223
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2024-04-05 22:00:54 +0530

    add ajax and validate request and details validate

--- smartGrader/webApp/urls.py
+++ smartGrader/webApp/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('',views.index, name='index'), #index page path(landing page)
     path('login/',views.login, name = 'login'), #login page path(landing page)
+    path('validate-login/', views.login_form_validate, name = 'validate_login') # login form validate
 ]
--- smartGrader/webApp/views.py
+++ smartGrader/webApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.template import loader
+from django.http import JsonResponse # use for get json resposes.
 
 # Create your views here.
 
@@ -10,3 +11,16 @@ def index(request):
 # add login page view 
 def login(request):
     return render(request, 'login.html')
+
+def login_form_validate(request):
+    if request.method == 'POST':
+        username = request.POST.get('mail')
+        password = request.POST.get('password')
+        
+        # Perform validation (Example: Check if username and password match)
+        if username == 'admin' and password == 'admin':
+            # Credentials are valid
+            return JsonResponse({'success': True})
+        else:
+            # Credentials are not valid
+            return JsonResponse({'success': False, 'message': 'Invalid username or password'}, status=400)
